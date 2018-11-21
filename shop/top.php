9550f980ac86960d9fcd7f1e1129a3bf539061d4
linshantang <812813303@qq.com>
2018-11-21 16:24:52 +0800

    add php pages
    add php pages

---
+++ shop/top.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<meta name="keywords" content="婚纱专卖、礼服专卖、婚纱礼服专卖、晚礼服专卖、高级订制、礼服订做、礼服定做、婚纱订做、婚纱定做、北京礼服、新娘礼服、新娘婚纱、婚纱、礼服、定做婚纱礼服、订做婚纱礼服、北京婚纱、高级定做、高级订做婚纱礼服、晚礼服、婚纱用品、北京婚纱礼服、中国婚纱、中国婚纱网、婚纱设计、婚纱设计网、婚纱礼服、婚纱礼服网、礼服设计、礼服设计网、bridesmaids、bridal gowns、bridalwear、eveningwear、Bride、jade bridal、bridal、exclusive、femail、beijing、newyork、london、paris、milan、wedding、wedding dress,china,dress,evening dress,gowns">
+<meta name="description" content="礼服订做、礼服定做、婚纱订做、婚纱定做、北京礼服、新娘礼服、新娘婚纱、婚纱、礼服、定做婚纱礼服、订做婚纱礼服、北京婚纱、高级定做、高级订做婚纱礼服、晚礼服、婚纱用品、北京婚纱礼服、中国婚纱、中国婚纱网、婚纱设计、婚纱设计网、婚纱礼服、婚纱礼服网、礼服设计、礼服设计网、 bridesmaids、bridal gowns、bridalwear、eveningwear、Bride、jade bridal、bridal、exclusive、femail、beijing、newyork、london、paris、milan、wedding、wedding dress,china,dress,evening dress,gowns">
+
+<title>红棠衣城</title>
+<link rel="stylesheet" type="text/css" href="css/font.css">
+<SCRIPT src="style/article.js"></SCRIPT>
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+<style type="text/css">
+body {
+	margin-top: 0px;
+}
+</style>
+</head>
+<body>
+<table width="766" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td height="81" colspan="2" valign="bottom" background="images/bannertop.png"><table width="766"  height="78" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="204" height="54">&nbsp;</td>
+        <td width="562"> <div align="right">
+		<SCRIPT language="JavaScript">
+<!-----------
+   var enabled = 0;   today = new Date();
+   var day;   var date;
+   if(today.getDay()==0)     day = "星期日"
+   if(today.getDay()==1)     day = "星期一"
+   if(today.getDay()==2)     day = "星期二"
+   if(today.getDay()==3)     day = "星期三"
+   if(today.getDay()==4)     day = "星期四"
+   if(today.getDay()==5)     day = "星期五"
+   if(today.getDay()==6)     day = "星期六"
+   document.fgColor = "A70977";
+   date1 = "<font size=2 color=black>" + (today.getYear())  + "年" + (today.getMonth() + 1 ) + "月" + today.getDate() + "日  " + "</font>";
+   date2 = "<font size=1 color=black>" +  day + "</font>";
+   document.write("<left>" + date1.fontsize(3) + date2.fontsize(3) + "</left>");
+   
+//----->
+</SCRIPT>  </div>   
+</td>
+      </tr>
+      <tr>
+        <td height="24">&nbsp;</td>
+        <td>&nbsp;<a href="index.php">首　　页</a>&nbsp;┊ <a href="shownewpr.php">最新服装</a> ┊ <a href="showtuijian.php">推荐品牌</a> ┊ <a href="showhot.php">热门品牌</a>&nbsp;┊&nbsp;<a href="showfenlei.php">产品分类</a>&nbsp;┊&nbsp;<a href="usercenter.php">用户中心</a>&nbsp;┊&nbsp;<a href="finddd.php">订单查询</a>&nbsp;┊&nbsp;<a href="gouwuche.php">购物车</a></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td width="219" height="32"><img src="images/bannerleft.png" width="219" height="144"></td>
+    <td width="547"><img src="images/bannerright.png" width="548" height="144"/></td>
+  </tr>
+</table>	
+
+<script type="text/javascript">
+swfobject.registerObject("FlashID");
+</script>
---
+++ shop/top1.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<meta name="keywords" content="婚纱专卖、礼服专卖、婚纱礼服专卖、晚礼服专卖、高级订制、礼服订做、礼服定做、婚纱订做、婚纱定做、北京礼服、新娘礼服、新娘婚纱、婚纱、礼服、定做婚纱礼服、订做婚纱礼服、北京婚纱、高级定做、高级订做婚纱礼服、晚礼服、婚纱用品、北京婚纱礼服、中国婚纱、中国婚纱网、婚纱设计、婚纱设计网、婚纱礼服、婚纱礼服网、礼服设计、礼服设计网、bridesmaids、bridal gowns、bridalwear、eveningwear、Bride、jade bridal、bridal、exclusive、femail、beijing、newyork、london、paris、milan、wedding、wedding dress,china,dress,evening dress,gowns">
+<meta name="description" content="礼服订做、礼服定做、婚纱订做、婚纱定做、北京礼服、新娘礼服、新娘婚纱、婚纱、礼服、定做婚纱礼服、订做婚纱礼服、北京婚纱、高级定做、高级订做婚纱礼服、晚礼服、婚纱用品、北京婚纱礼服、中国婚纱、中国婚纱网、婚纱设计、婚纱设计网、婚纱礼服、婚纱礼服网、礼服设计、礼服设计网、 bridesmaids、bridal gowns、bridalwear、eveningwear、Bride、jade bridal、bridal、exclusive、femail、beijing、newyork、london、paris、milan、wedding、wedding dress,china,dress,evening dress,gowns">
+
+<title>翡翠电子商城</title>
+<link rel="stylesheet" type="text/css" href="css/font.css">
+<SCRIPT src="style/article.js"></SCRIPT>
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+<style type="text/css">
+body {
+	margin-top: 0px;
+}
+</style>
+</head>
+<body>
+<table width="766" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td height="81" colspan="2" valign="bottom" background="images/bannertop.gif"><table width="766"  height="78" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="204" height="54">&nbsp;</td>
+        <td width="562"> <div align="right">
+		<SCRIPT language="JavaScript">
+<!-----------
+   var enabled = 0;   today = new Date();
+   var day;   var date;
+   if(today.getDay()==0)     day = "星期日"
+   if(today.getDay()==1)     day = "星期一"
+   if(today.getDay()==2)     day = "星期二"
+   if(today.getDay()==3)     day = "星期三"
+   if(today.getDay()==4)     day = "星期四"
+   if(today.getDay()==5)     day = "星期五"
+   if(today.getDay()==6)     day = "星期六"
+   document.fgColor = "A70977";
+   date1 = "<font size=2 color=black>" + (today.getYear())  + "年" + (today.getMonth() + 1 ) + "月" + today.getDate() + "日  " + "</font>";
+   date2 = "<font size=1 color=black>" +  day + "</font>";
+   document.write("<left>" + date1.fontsize(3) + date2.fontsize(3) + "</left>");
+   
+//----->
+</SCRIPT>  </div>   
+</td>
+      </tr>
+      <tr>
+        <td height="24">&nbsp;</td>
+        <td>&nbsp;<a href="index.php">首　　页</a>&nbsp;┊ <a href="shownewpr.php">最新服装</a> ┊ <a href="showtuijian.php">推荐品牌</a> ┊ <a href="showhot.php">热门品牌</a>&nbsp;┊&nbsp;<a href="showfenlei.php">产品分类</a>&nbsp;┊&nbsp;<a href="usercenter.php">用户中心</a>&nbsp;┊&nbsp;<a href="finddd.php">订单查询</a>&nbsp;┊&nbsp;<a href="gouwuche.php">购物车</a></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td width="219" height="32"><img src="images/bannerleft.gif" width="219" height="144"></td>
+    <td width="547"><object id="FlashID" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="547" height="144">
+      <param name="movie" value="flash/banner.swf">
+      <param name="quality" value="high">
+      <param name="wmode" value="opaque">
+      <param name="swfversion" value="6.0.65.0">
+      <!-- 此 param 标签提示使用 Flash Player 6.0 r65 和更高版本的用户下载最新版本的 Flash Player。如果您不想让用户看到该提示，请将其删除。 -->
+      <param name="expressinstall" value="Scripts/expressInstall.swf">
+      <!-- 下一个对象标签用于非 IE 浏览器。所以使用 IECC 将其从 IE 隐藏。 -->
+      <!--[if !IE]>-->
+      <object type="application/x-shockwave-flash" data="flash/banner.swf" width="547" height="144">
+        <!--<![endif]-->
+        <param name="quality" value="high">
+        <param name="wmode" value="opaque">
+        <param name="swfversion" value="6.0.65.0">
+        <param name="expressinstall" value="Scripts/expressInstall.swf">
+        <!-- 浏览器将以下替代内容显示给使用 Flash Player 6.0 和更低版本的用户。 -->
+        <div>
+          <h4>此页面上的内容需要较新版本的 Adobe Flash Player。</h4>
+          <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="获取 Adobe Flash Player" width="112" height="33" /></a></p>
+        </div>
+        <!--[if !IE]>-->
+      </object>
+      <!--<![endif]-->
+    </object></td>
+  </tr>
+</table>	
+
+<script type="text/javascript">
+swfobject.registerObject("FlashID");
+</script>
